5648ccf665c2d14300313526402c20226cccd96f
sduong34 <sarahduong34@gmail.com>
2022-09-18 22:45:45 -0500

    1

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('p1_about.html')
+
+@app.route('/services')
+def services():
+    return render_template('p2_services.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
